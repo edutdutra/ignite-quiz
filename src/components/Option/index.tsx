f3c6bf05f20dd80a914a2e91776b49d5a927ecfc
edutdutra <edutdutra@gmail.com>
2024-01-16 09:27:26 -0300

    add react native skia effects and animation

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,26 +1,81 @@
-import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import {Text, TouchableOpacity, TouchableOpacityProps} from 'react-native';
 
-import { styles } from './styles';
+import {styles} from './styles';
+import {BlurMask, Canvas, Circle, Path, runTiming, Skia, useValue, Easing} from "@shopify/react-native-skia";
+import {THEME} from "../../styles/theme";
+import {useEffect} from "react";
 
 type Props = TouchableOpacityProps & {
-  checked: boolean;
-  title: string;
+    checked: boolean;
+    title: string;
 }
 
-export function Option({ checked, title, ...rest }: Props) {
-  return (
-    <TouchableOpacity
-      style={
-        [
-          styles.container,
-          checked && styles.checked
-        ]
-      }
-      {...rest}
-    >
-      <Text style={styles.title}>
-        {title}
-      </Text>
-    </TouchableOpacity>
-  );
+const CHECK_SIZE = 28;
+const CHECK_STROKE = 2;
+const RADIUS = (CHECK_SIZE - CHECK_STROKE) / 2;
+const CENTER_CIRCLE = RADIUS / 2;
+
+export function Option({checked, title, ...rest}: Props) {
+    const percentage = useValue(0);
+    const circle = useValue(0);
+    const path = Skia.Path.Make();
+
+    path.addCircle(CHECK_SIZE, CHECK_SIZE, RADIUS)
+
+
+    useEffect(() => {
+        if (checked) {
+            runTiming(percentage, 1, {duration: 700})
+            runTiming(circle, CENTER_CIRCLE, {easing: Easing.bounce})
+        } else {
+            runTiming(percentage, 0, {duration: 700})
+            runTiming(circle, 0, {duration: 300})
+        }
+    }, [checked]);
+
+    return (
+        <TouchableOpacity
+            style={
+                [
+                    styles.container,
+                    checked && styles.checked
+                ]
+            }
+            {...rest}
+        >
+            <Text style={styles.title}>
+                {title}
+            </Text>
+            <Canvas style={{height: CHECK_SIZE * 2, width: CHECK_SIZE * 2}}>
+                <Path
+                    path={path}
+                    start={0}
+                    end={1}
+                    color={THEME.COLORS.GREY_500}
+                    style="stroke"
+                    strokeWidth={CHECK_STROKE}
+                />
+
+                <Path
+                    path={path}
+                    start={0}
+                    end={percentage}
+                    color={THEME.COLORS.BRAND_LIGHT}
+                    style="stroke"
+                    strokeWidth={CHECK_STROKE}
+                >
+                    <BlurMask style="solid" blur={1} />
+                </Path>
+
+                <Circle
+                    r={circle}
+                    cx={CHECK_SIZE}
+                    cy={CHECK_SIZE}
+                    color={THEME.COLORS.BRAND_LIGHT}
+                >
+                    <BlurMask style="solid" blur={3} />
+                </Circle>
+            </Canvas>
+        </TouchableOpacity>
+    );
 }
--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -26,6 +26,7 @@ import Animated, {
 import {ProgressBar} from "../../components/ProgressBar";
 import {THEME} from "../../styles/theme";
 import {Gesture, GestureDetector} from "react-native-gesture-handler";
+import {OverlayFeedback} from "../../components/OverlayFeedback";
 
 interface Params {
     id: string;
@@ -42,6 +43,7 @@ export function Quiz() {
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [quiz, setQuiz] = useState<QuizProps>({} as QuizProps);
     const [alternativeSelected, setAlternativeSelected] = useState<null | number>(null);
+    const [statusReply, setStatusReply] = useState<number>(0);
 
     const {navigate} = useNavigation();
 
@@ -124,8 +126,11 @@ export function Quiz() {
         }
 
         if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+            setStatusReply(1)
             setPoints(prevState => prevState + 1);
+            handleNextQuestion();
         } else {
+            setStatusReply(2)
             shakeAnimation();
         }
 
@@ -149,7 +154,15 @@ export function Quiz() {
     }
 
     function shakeAnimation() {
-        shake.value = withSequence(withTiming(3, {duration: 400, easing: Easing.bounce}), withTiming(0));
+        shake.value = withSequence(
+            withTiming(3, {duration: 400, easing: Easing.bounce}),
+            withTiming(0, undefined, (finished) => {
+                'worklet';
+                if (finished) {
+                    runOnJS(handleNextQuestion)();
+                }
+            })
+        );
     }
 
     const scrollHandler = useAnimatedScrollHandler({
@@ -180,11 +193,11 @@ export function Quiz() {
         setIsLoading(false);
     }, []);
 
-    useEffect(() => {
-        if (quiz.questions) {
-            handleNextQuestion();
-        }
-    }, [points]);
+    // useEffect(() => {
+    //     if (quiz.questions) {
+    //         handleNextQuestion();
+    //     }
+    // }, [points]);
 
     if (isLoading) {
         return <Loading/>
@@ -192,6 +205,8 @@ export function Quiz() {
 
     return (
         <View style={styles.container}>
+            <OverlayFeedback status={statusReply}/>
+
             <Animated.View style={fixedProgressBarStyles}>
                 <Text style={styles.title}>
                     {quiz.title}
@@ -220,6 +235,7 @@ export function Quiz() {
                             question={quiz.questions[currentQuestion]}
                             alternativeSelected={alternativeSelected}
                             setAlternativeSelected={setAlternativeSelected}
+                            onUnmount={() => setStatusReply(0)}
                         />
                     </Animated.View>
                 </GestureDetector>
